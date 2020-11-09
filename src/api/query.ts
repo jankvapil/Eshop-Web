fc4193864692c0eb75d076bb49b9682ddb46c9d7
unknown <jkvapil6@gmail.com>
2020-11-09 15:47:39 +0100

    first

---
+++ src/api/query.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const GRAPHQL_API = "http://localhost:5000/graphql"
+
+export const query = async (query: string) => {
+  console.log(`Sending query: `)
+  console.log(query)
+
+  try {
+    const queryResult = await axios.post(
+      GRAPHQL_API,
+      {
+        headers: {"Access-Control-Allow-Origin": '*'},
+        query: query
+      }
+    )
+  
+    return queryResult.data.data
+  
+  } catch (error) {
+    alert(error)
+    console.error(error)
+    return null
+  }
+}
